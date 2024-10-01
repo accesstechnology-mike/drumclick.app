c03cb4db362f02316384e4d86d8f1b7497d34d27
Mike Laptop <mike@accesstechnology.co.uk>
2024-10-01 20:58:41 +0100

    disabling eslint during builds

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // ... other config options ...
+}
 
-export default nextConfig;
+module.exports = nextConfig
